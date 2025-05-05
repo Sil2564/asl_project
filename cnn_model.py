88cf79dc1595945fdfe38c734e7d8b4844350db4
sil <silvia.busti@studio.unibo.it>
2025-05-05 13:32:13 +0200

    speriamo

--- cnn_model.py
+++ cnn_model.py
@@ -6,8 +6,8 @@ class SimpleCNN(nn.Module):
     def __init__(self, num_classes=26):  # Numero di classi nel dataset ASL (alfabeto inglese)
         super(SimpleCNN, self).__init__()
 
-        # Strato convoluzionale 1
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1)
+        # Strato convoluzionale 1 (modificato per 3 canali)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         # Strato convoluzionale 2

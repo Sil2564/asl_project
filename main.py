b95e701c963c8d2c4d1a391a3019e3d2822ba6f3
sil <silvia.busti@studio.unibo.it>
2025-05-05 11:57:46 +0200

    Aggiunto il codice per la CNN e il training

---
+++ main.py
@@ -0,0 +1,11 @@
+from train import train
+from cnn_model import SimpleCNN
+
+# Impostazioni
+num_epochs = 10
+
+# Crea il modello
+model = SimpleCNN(num_classes=10)  # 10 classi per il dataset MNIST
+
+# Avvia il training
+train(model, num_epochs)

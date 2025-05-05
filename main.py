19d8e172c95a679d7bc2ad20dc01d0c10ce7107e
sil <silvia.busti@studio.unibo.it>
2025-05-05 13:01:01 +0200

    corretto forse

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from custom_dataset import get_dataloaders
 from cnn_model import SimpleCNN
 from train import train
 import torch
 import torch.nn as nn
 import torch.optim as optim
+
+# Ottieni i DataLoader
+train_loader, val_loader, test_loader = get_dataloaders(data_dir='dataset', batch_size=64, image_size=64)
 
 # Istanzia il modello
 model = SimpleCNN()
@@ -17,5 +21,4 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 num_epochs = 10
 
 # Avvia il training
-train(model, criterion, optimizer, num_epochs)
-
+train(model, train_loader, criterion, optimizer, num_epochs)

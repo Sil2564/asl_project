f6104e589a97bbc800a551994a029b5d080a2793
sil <silvia.busti@studio.unibo.it>
2025-05-05 13:48:20 +0200

    dc

--- custom_dataset.py
+++ custom_dataset.py
@@ -1,27 +1,20 @@
 import os
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-from PIL import Image
 
 def get_dataloaders(data_dir='dataset', batch_size=32, image_size=64):
     # Trasformazioni da applicare alle immagini
     transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),  # Ridimensiona tutte le immagini
+        transforms.Grayscale(num_output_channels=3),  # Forza la conversione in RGB
         transforms.ToTensor(),  # Converte in tensore PyTorch
-        transforms.Normalize((0.5,), (0.5,))  # Normalizzazione tra -1 e 1
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalizzazione tra -1 e 1
     ])
 
-    # Funzione per garantire che le immagini siano RGB
-    def rgb_loader(path):
-        img = Image.open(path)
-        if img.mode != 'RGB':  # Se l'immagine non è RGB, convertila
-            img = img.convert('RGB')
-        return img
-
-    # Crea dataset con il loader personalizzato che assicura l'uso di immagini RGB
-    train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), transform=transform, loader=rgb_loader)
-    val_dataset = datasets.ImageFolder(os.path.join(data_dir, 'val'), transform=transform, loader=rgb_loader)
-    test_dataset = datasets.ImageFolder(os.path.join(data_dir, 'test'), transform=transform, loader=rgb_loader)
+    # Crea dataset
+    train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), transform=transform)
+    val_dataset = datasets.ImageFolder(os.path.join(data_dir, 'val'), transform=transform)
+    test_dataset = datasets.ImageFolder(os.path.join(data_dir, 'test'), transform=transform)
 
     # Crea DataLoader
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
